613318954eb35374562cfb923855c162ae964c1f
preeti-dudi <64349037+preeti-dudi@users.noreply.github.com>
2020-12-19 20:29:06 +0530

    somechanges

--- wearehere/javascript.js
+++ wearehere/javascript.js
@@ -1,11 +1,11 @@
 window.onload=function(){
     var el=document.getElementById("intro");
-    var str ='HELP IN COVID              We are here';
+    var str ='WE ARE HERE';
     var i=0;
     el.innerHTML="";
     var text=setInterval(function(){if(i==(str.length)){clearInterval(text);}else{el.innerHTML=el.innerHTML+str[i];i++;}},500);
     var ele=document.getElementById("intro1");
-    var stri="                World is fighting covid.                 We are not only losing lives. But also jobs, health, education.                At wearehere, our organization, we try to provide others a mean to fight these.                We want to impact the world, help the world, make it better.                We want to be the HOPE.";
+    var stri="World is fighting covid. We are not only losing lives. But also jobs, health, education. At wearehere, our organization, we try to provide others a mean to fight these. We want to impact the world, help the world, make it better. We want to be the HOPE.";
     var j=0;
     el.innerHTML="";
     var test=setInterval(function(){if(j==(stri.length)){clearInterval(test);}else{ele.innerHTML=ele.innerHTML+stri[j];j++;}},100);
@@ -42,4 +42,19 @@ function needWork(){
     h1.appendChild(node)
     div.appendChild(h1);
     document.body.appendChild(div);
+}
+
+var images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
+var i=0;
+var img=document.getElementById("image");
+var Scr="assets/images/";
+function next(){
+  i++;
+  Src+=images[i];
+  img.src=Src;
+}
+function prev(){
+  i--;
+  Src+=images[i];
+  img.src=Src;
 }
